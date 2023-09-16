1a27c188f21f5985ed042da33f9ca09b05fb4d29
geopokrovskiy <99304283+geopokrovskiy@users.noreply.github.com>
2023-09-16 09:19:02 +0200

    Update of the project (authentication chain added)

---
+++ src/main/java/com/geopokrovskiy/rest/AuthRestControllerV1.java
@@ -0,0 +1,52 @@
+package com.geopokrovskiy.rest;
+
+import com.geopokrovskiy.dto.AuthRequestDto;
+import com.geopokrovskiy.dto.AuthResponseDto;
+import com.geopokrovskiy.dto.UserDto;
+import com.geopokrovskiy.entity.UserEntity;
+import com.geopokrovskiy.mapper.UserMapper;
+import com.geopokrovskiy.security.CustomPrincipal;
+import com.geopokrovskiy.security.SecurityService;
+import com.geopokrovskiy.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthRestControllerV1 {
+    private final SecurityService securityService;
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+
+    @PostMapping("/register")
+    public Mono<UserDto> register(@RequestBody UserDto dto) {
+        UserEntity entity = userMapper.map(dto);
+        return userService.registerUser(entity)
+                .map(userMapper::map);
+    }
+
+    @PostMapping("/login")
+    public Mono<AuthResponseDto> login(@RequestBody AuthRequestDto dto) {
+        return securityService.authenticate(dto.getUsername(), dto.getPassword())
+                .flatMap(tokenDetails -> Mono.just(
+                        AuthResponseDto.builder()
+                                .userId(tokenDetails.getUserId())
+                                .token(tokenDetails.getToken())
+                                .issuedAt(tokenDetails.getIssuedAt())
+                                .expiresAt(tokenDetails.getExpiresAt())
+                                .build()
+                ));
+    }
+
+    @GetMapping("/info")
+    public Mono<UserDto> getUserInfo(Authentication authentication) {
+        CustomPrincipal customPrincipal = (CustomPrincipal) authentication.getPrincipal();
+
+        return userService.getUserById(customPrincipal.getId())
+                .map(userMapper::map);
+    }
+}
